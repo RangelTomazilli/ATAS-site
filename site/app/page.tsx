d04323b22ad5ebf09b2ba0a09c1cc7dcef96c4a9
Rangel <rangel.tomazilli@gmail.com>
2025-02-03 13:39:56 -0300

    removendo descrição pg principal.

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -6,52 +6,52 @@ export default function Home() {
     {
       image: "/cards-images/hatch.png",
       title: "Hatch",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
     {
       image: "/cards-images/sedan.png",
       title: "Sedan",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
     {
       image: "/cards-images/SUV.png",
       title: "SUV",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
     {
       image: "/cards-images/pick-up.png",
       title: "Pick-up",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
     {
       image: "/cards-images/7lugares.png",
       title: "7 Lugares",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
     {
       image: "/cards-images/ambulancia.png",
       title: "Ambulâncias",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
     {
       image: "/cards-images/van.png",
       title: "Vans",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
     {
       image: "/cards-images/bus.png",
       title: "Ônibus",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
     {
       image: "/cards-images/caminhao.png",
       title: "Caminhões",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
     {
       image: "/cards-images/trator.png",
       title: "Tratores",
-      description: "Lorem ipsum dolor sit amet consectetur. Sed aliquam pharetra ut.",
+      description: "",
     },
   ];
   
