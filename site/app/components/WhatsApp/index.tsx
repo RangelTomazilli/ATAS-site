ea1a7e9ac601415d1e149e9e1e8656dd22ae3294
Rangel <rangel.tomazilli@gmail.com>
2025-02-03 13:34:13 -0300

    tel correto

--- site/app/components/WhatsApp/index.tsx
+++ site/app/components/WhatsApp/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const WhatsAppButton: React.FC = () => {
   return (
     <a
-      href="https://wa.me/5531987425572"
+      href="https://wa.me/553130268850"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 flex items-center justify-center w-20 h-20 bg-green-500 rounded-full shadow-lg hover:scale-110 transition-transform duration-300 animate-pulse"
