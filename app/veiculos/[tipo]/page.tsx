6bc20fe6527ff1b60603309587227cb6ac6a6128
Rangel <rangel.tomazilli@gmail.com>
2025-04-02 12:36:45 -0300

    novos veículos.

--- app/veiculos/[tipo]/page.tsx
+++ app/veiculos/[tipo]/page.tsx
@@ -7,42 +7,59 @@ export default function Page () {
   const params = useParams();
   const tipo = params?.tipo as string;
   const veiculosData = {
-    "hatch": [
+    hatch: [
       {
         nome: "CITROEN C3 LIVE",
         valor: "R$ 77.900,00",
         descricao: (
           <>
             <p>
-            {`Veículo automotor, hatch, capacidade de 05 lugares (sendo 4 passageiros e 1 motorista), ano/modelo 2024/2024 ou superior, cor branca, com motor flex (gasolina/etanol) com potência mínima de 70 cv, ar-condicionado, direção elétrica ou hidráulica, trava elétrica nas portas, vidros elétricos nas portas dianteiras, para-choques pintados na cor do veículo, porta-malas de no mínimo 300 litros e com todos os itens de segurança de acordo com o Código de Trânsito Brasileiro e exigidos pelo Denatran.`}
+              {`Veículo automotor, hatch, capacidade de 05 lugares (sendo 4 passageiros e 1 motorista), ano/modelo 2024/2024 ou superior, cor branca, com motor flex (gasolina/etanol) com potência mínima de 70 cv, ar-condicionado, direção elétrica ou hidráulica, trava elétrica nas portas, vidros elétricos nas portas dianteiras, para-choques pintados na cor do veículo, porta-malas de no mínimo 300 litros e com todos os itens de segurança de acordo com o Código de Trânsito Brasileiro e exigidos pelo Denatran.`}
             </p>
           </>
         ),
       },
       {
-        nome: "RENAULT KWID ZEN 1.0",
-        valor: "R$ 74.000,00",
+        nome: "FIAT MOBI LIKE 1.0 2025",
+        valor: "R$ 80.000,00",
         descricao: (
           <>
             <p>
-            {`VEÍCULO HATCH ANO/MOD: 2024
-Configurações mínimas: Combustível: Flex, Cilindrada (cc): 999,1, Potência, (cv) : 68(G)/71(E), Torque: 9,3(G)/9,7(E), Altura do solo (mm): 175, Altura do veículo (mm): 1.400, Capacidade do porta-malas: (litros): 270, Comprimento do veículo (mm): 3.500, Distância entre-eixos : 2.300, Tanque de combustível (litros): 38, Ar condicionado, Banco traseiro rebatível, Cinto de segurança traseiro retrátil 3 pontos, Cintos de segurança dianteiros retráteis de 3 pontos,
-.Computador de Bordo, Direção hidráulica, Sinalização de frenagem de emergência, Airbag duplo (motorista e passageiro) e Freios ABS, Retrovisores externos com comando interno mecânico, Rodas de aço estampado 5.5 x 14" com calotas integrais + Pneus 165/70 R14, Vidros elétricos dianteiros e anti-esmagamento) e travas elétricas nas 4 portas, Radio am/fm com entrada USB e
-Bluetooth.
-`}
+              {`VEICULO HATCH 1.0: 
+              Capacidade mininas ;
+              Cor metalica;
+              motor 1.0;
+              Cilindra 999,1; 
+              Potencia 66,00 cv; 
+              Porta malas: 200 litros; 
+              Entre eixo: 2.300; 
+              Tanque: 36 litros;
+              Ar condicionado; 
+              Direção hidráulica ou elétrica ou eletro hidráulica. `}
+            </p>
+          </>
+        ),
+      },
+      {
+        nome: "VOLKSWAGEN POLO SENSE TSI",
+        valor: "R$ 102.200,00",
+        descricao: (
+          <>
+            <p>
+              {`VEICULO HATCH/SUV - VEICULO DE PASSEIO - TRANSPORTE DE EQUIPE (5 PESSOAS, 0KM) 1.0 A 1.6 CAPACIDADE MÍNIMAS: Cor metalica motor 1.0 potencia minima 98,00 cv porta malas: 285 litros; distancia entre os eixos minima: 2.370; tanque: 40 litros - ar condicionado – trio elétrico, direção hidráulica ou elétrica ou eletro hidráulica; fabricado no maximo ha 6 meses, com todos os acessorios minimos obrigatorios, conforme legislacao em vigor.`}
             </p>
           </>
         ),
       },
     ],
-    "suv": [
+    suv: [
       {
-        nome: "FIAT / FASTBACK TURBO",
-        valor: "R$ 123.000,00",
+        nome: "VOLKSWAGEN POLO SENSE TSI",
+        valor: "R$ 102.200,00",
         descricao: (
           <>
             <p>
-            {`Cilindrada total (cc) : 999, Potência (cv) : 125,0 (G) / 130,0 (E), Altura do veículo (mm) : 1500, Capacidade do porta-malas (litros) : 360, Comprimento do veículo (mm) : 4000, Entre-Eixos (mm) : 2500, Tanque de combustível (litros) : 46, Banco traseiro bipartido 60/40 e com assentos rebatíveis, *Roda de liga leve R16, Airbags (4) - Frontal (2), Tórax e Cabeça (2), .larme antifurto, Ar-condicionado digital, ASR (Controle eletrônico de tração), Banco do motorista com regulagem de altura Brake light, Câmbio automático CVT (7 velocidades, simuladas), Central multimídia com tela de 8,0" touchscreen, Apple Car Play e Android Auto wireless, Comandos de Voz, Bluetooth, MP3, Rádio AM/FM, Comandos de áudio e painel de instrumentos no volante, Computador de Bordo (distância, consumo médio, consumo instantâneo, autonomia, velocidade média e tempo de percurso), Desembaçador do vidro traseiro com temporizador, Direção elétrica, ESP (Controle Eletrônico de Estabilidade) ESS (Sinalização de frenagem de emergência), Hill holder (Assistente de partida em rampa), Retrovisores externos com regulagem elétrica, Sensor de estacionamento traseiro, Vidros elétricos dianteiros e traseiros com função antiesmagamento, Volante com regularem de altura.`}
+              {`Cilindrada total (cc) : 999, Potência (cv) : 125,0 (G) / 130,0 (E), Altura do veículo (mm) : 1500, Capacidade do porta-malas (litros) : 360, Comprimento do veículo (mm) : 4000, Entre-Eixos (mm) : 2500, Tanque de combustível (litros) : 46, Banco traseiro bipartido 60/40 e com assentos rebatíveis, *Roda de liga leve R16, Airbags (4) - Frontal (2), Tórax e Cabeça (2), .larme antifurto, Ar-condicionado digital, ASR (Controle eletrônico de tração), Banco do motorista com regulagem de altura Brake light, Câmbio automático CVT (7 velocidades, simuladas), Central multimídia com tela de 8,0" touchscreen, Apple Car Play e Android Auto wireless, Comandos de Voz, Bluetooth, MP3, Rádio AM/FM, Comandos de áudio e painel de instrumentos no volante, Computador de Bordo (distância, consumo médio, consumo instantâneo, autonomia, velocidade média e tempo de percurso), Desembaçador do vidro traseiro com temporizador, Direção elétrica, ESP (Controle Eletrônico de Estabilidade) ESS (Sinalização de frenagem de emergência), Hill holder (Assistente de partida em rampa), Retrovisores externos com regulagem elétrica, Sensor de estacionamento traseiro, Vidros elétricos dianteiros e traseiros com função antiesmagamento, Volante com regularem de altura.`}
             </p>
           </>
         ),
@@ -51,38 +68,56 @@ Bluetooth.
     "pick-up": [
       {
         nome: "FIAT / TORO VOLCANO TURBODIESEL 4X4",
-        valor: "R$ 191.000,00",
+        valor: "R$ 212.000,00",
         descricao: (
           <>
             <p className="mt-4">
-            {`VEICULO CAMINHONETE CABINE DUPLA DIESEL ANO/MOD:2024
-            Combustível: Diesel, Cilindrada total (cc): 1.956, Potência mínima (cv): 160, Torque mínimo (kgf.m): 32,7 @ 1.750 rpm, Altura do veículo (mm): 1.600, Capacidade de carga (Kg): 1.000, Comprimento do veículo (mm) : 4.900, Entre-Eixos, mm): 2.900, Tanque de combustível (litros): 60, Banco do motorista elétrico, Bancos em Couro, âmbio automático de mínimo 7 velocidades, Tração (4x4 low), Rodas em liga leve 6.5 x 18 + pneus 225/60 R18, Sensor de estacionamento Frontal, Tração 4x4 com seletor, Espelho no para-sol lados motorista e passageiro, Abertura elétrica do bocal de abastecimento, Airbags (6) - laterais (2) cortina (2) Frontal (2), Alarme antifurto. Alça de segurança traseira com luz de leitura incorporada. Alto falantes dianteiros (2), Alto falantes traseiros (2) e Antena. Ar-condicionado digital. ASR (Controle de Tração). Banco do motorista com regulagem de altura. Barras longitudinais no teto, Brake light, Câmera de ré. Capota marítima. Central multimídia com tela de mínimo 7,0" touchscreen; Apple CarPlay wireless e Android Auto wireless; Comandos de voz Bluetooth, MP3, Rádio AM/FM, entrada aux, Porta USB, ESP (Controle Eletrônico de Estabilidade). Freios ABS com EBD. Ganchos para amarração de carga na caçamba, .Hill Holder (sistema ativo freio com controle eletrônico que auxilia nas arrancadas do veículo em subida). Retrovisores externos com comando elétrico, Sensor de estacionamento traseiro, Comandos de áudio no volante, Computador de bordo (distância, velocidade média e tempo de percurso). Desembaçador do vidro traseiro. Direção elétrica. Dois Tweeters. Travas elétricas (Travamento automático a 20 km/h, indicador de portas abertas, luz inter na com temporizador e tampa do combustível), Vidros elétricos dianteiros e traseiros antiesmagamento, Volante com regulagem de altura e profundidade.`}
+              {`VEICULO CAMINHONETE CABINE DUPLA DIESEL 
+              Capacidade mínimas 
+              Cor metalica 
+              Motor 2.0 no minimo 
+              Lugares: 5 
+              Cilindra 1.900 
+              Potencia 170,00 cv Motor: DIESEL 
+              Entre eixo: 2.900 Capacidade de carga: 900 kg 
+              Tanque: 55 litros 
+              Ar condicionado 
+              Direção hidráulica ou elétrica ou eletro hidráulica;`}
             </p>
           </>
         ),
       },
       {
-        nome: "RENAULT OROCH PRO 1.6",
-        valor: "R$ 120.000,00",
+        nome: "VOLKSWAGEN NOVA AMAROK V6 COMFORTLINE",
+        valor: "R$ 298,000,00",
         descricao: (
           <>
             <p className="mt-4">
-            {`VEÍCULO PICK UP CABINE DUPLA
-ANO/MOD: 2024 Configurações mínimas: Tipo de carroceria PICK UP, Motor 1.6, Combustível Flex, Potência 118cv, Torque 15,0 kgfm, Número de cilindros 4 cilindros em linha, Cilindrada (cm³) 1.500 cm³, Direção Hidraulica, Tipo de tração Dianteira, Caixa de câmbio Manual Número de marchas 5 Dimensões Distância entre-eixos 2.800 mm Altura exterior 1.690 mm Comprimento exterior 4.700 mm, Número de lugares 5, Volume Volume da caçamba 680 Litros; Retrovisores regulagem manual; Ar-condicionado; Banco rebatível 1/1; Apoio de cabeça traseiro central e laterais com ajuste de altura; Tecnologia Rádio (Bluetooth, USB, AUX); Cintos de segurança traseiros de 3 pontos; Freios ABS; 2 airbags frontais; Câmbio manual de 5 velocidades; Roda aro 14".
-`}
+              {`CAMINHONETE PICK UP CABINE DUPLA DIESEL 5 LUGARES, VEICULO NOVO 2025.`}
+            </p>
+          </>
+        ),
+      },
+      {
+        nome: "FIAT STRADA ULTRA CABINE DUPLA",
+        valor: "R$ 137,800,00",
+        descricao: (
+          <>
+            <p className="mt-4">
+              {`CAMINHONETE PICK UP PEQUENA CAB DUPLA FLEX, VEICULO NOVO 2025`}
             </p>
           </>
         ),
       },
     ],
-    "tratores": [
+    tratores: [
       {
         nome: "Bundny BDY 8540 SL Cabinado",
         valor: "R$ 213.000,00",
         descricao: (
           <>
             <p>
-            {`TRATOR AGRÍCOLA DE PNEUS, com as seguintes características: NOVO, ano e modelo igual ou superior a 2024, ZERO KM, tração 4X4, com cabine fechada ROPS/FOPS original de fábrica, com acesso por um lado e com ar-condicionado, motor a diesel, turbo, 4 cilindros, padrão de emissões MAR I (Resolução CONAMA), potência igual a 85 CV, caixa de câmbio sincronizada com 12 velocidades à frente e 12 à ré, tomada de potência/força independente/eletrohidráulico, potência de levantamento hidráulico de 3.460 kg, pneus dianteiros 12.4-24” R1 e traseiros 18.4-30” R1, capacidade de tanque de 110 litros (com tanque auxiliar), tomada de força dupla, peso do trator de 3.600 kg, tomada de potência traseira 540 RPM, sistema hidráulico com capacidade de 51 litros/min, equipado com duas válvulas de controle remoto, freios hidráulicos com acionamento mecânico banhados a óleo, direção hidrostática, engate de três pontos, barra de tração e contrapesos de série, e equipado com todos os itens/acessórios de série, bem como os obrigatórios e exigidos pelo Código Nacional de Trânsito e INMETRO.`}
+              {`TRATOR AGRÍCOLA DE PNEUS, com as seguintes características: NOVO, ano e modelo igual ou superior a 2024, ZERO KM, tração 4X4, com cabine fechada ROPS/FOPS original de fábrica, com acesso por um lado e com ar-condicionado, motor a diesel, turbo, 4 cilindros, padrão de emissões MAR I (Resolução CONAMA), potência igual a 85 CV, caixa de câmbio sincronizada com 12 velocidades à frente e 12 à ré, tomada de potência/força independente/eletrohidráulico, potência de levantamento hidráulico de 3.460 kg, pneus dianteiros 12.4-24” R1 e traseiros 18.4-30” R1, capacidade de tanque de 110 litros (com tanque auxiliar), tomada de força dupla, peso do trator de 3.600 kg, tomada de potência traseira 540 RPM, sistema hidráulico com capacidade de 51 litros/min, equipado com duas válvulas de controle remoto, freios hidráulicos com acionamento mecânico banhados a óleo, direção hidrostática, engate de três pontos, barra de tração e contrapesos de série, e equipado com todos os itens/acessórios de série, bem como os obrigatórios e exigidos pelo Código Nacional de Trânsito e INMETRO.`}
             </p>
           </>
         ),
@@ -93,7 +128,7 @@ ANO/MOD: 2024 Configurações mínimas: Tipo de carroceria PICK UP, Motor 1.6, C
         descricao: (
           <>
             <p>
-            {`TRATOR AGRÍCOLA NOVO. ZERO HORA. Potência maior ou igual a 85 CV e menor que 99 CV. Plataformado. Tipo C. Especificações conforme folha de dados.`}
+              {`TRATOR AGRÍCOLA NOVO. ZERO HORA. Potência maior ou igual a 85 CV e menor que 99 CV. Plataformado. Tipo C. Especificações conforme folha de dados.`}
             </p>
           </>
         ),
@@ -102,13 +137,35 @@ ANO/MOD: 2024 Configurações mínimas: Tipo de carroceria PICK UP, Motor 1.6, C
     "7-lugares": [
       {
         nome: "CITROEN / C3 AIRCROSS 7 FEEL TURBO",
-        valor: "R$ 118.500,00",
+        valor: "R$ 118.800,00",
         descricao: (
           <>
             <p>
-            {`VEÍCULO AUTOMOTOR, CAPACIDADE DE 07 LUGARES (SENDO 6 PASSAGEIROS E 1 MOTORISTA), ANO/MODELO 2024/2024 OU SUPERIOR, COR BRANCA, COM MOTOR FLEX (GASOLINA / ETANOL) COM POTENCIA MÍNIMA 100 CVS DE POTÊNCIA, AR CONDICIONADO, DIREÇÃO ELÉTRICA OU HIDRÁULICA, TRAVA ELÉTRICA NAS PORTAS, VIDROS ELÉTRICOS NAS PORTAS DIANTEIRAS, PARA CHOQUES PINTADOS NA COR DO VEÍCULO, E COM TODOS OS ITENS DE SEGURANÇA DE ACORDO COM O CÓDIGO DE
-TRÂNSITO BRASILEIRO E EXIGIDOS PELO DENATRAN.`}
-            </p>            
+              {`Veículo Minivan (7 lugares): Veiculo biocombustível, direção hidráulica ou elétrica, 04 portas, Cor metalica, câmbio manual ou automático, Distância entre eixos mínima de 2.500mm, Motorização mínima 1.0 Ar condicionado, Trio Elétrico. Complementação 0km, fabricado, no máximo, há 6 (Seis) meses, com todos os acessórios mínimos obrigatórios, conforme legislação em vigor. `}
+            </p>
+          </>
+        ),
+      },
+    ],
+    sedan: [
+      {
+        nome: "VOLKSWAGEN VIRTUS SENSE TSI",
+        valor: "R$ 102.800,00",
+        descricao: (
+          <>
+            <p>
+              {`VEICULO SEDAN 1.0 Turbo ou 1.3 
+Capacidade mínimas 
+Cor metalica 
+motor 1.0 turbo 
+Cilindra 999,1; 
+Potencia 98,00 cv 
+Porta malas: 485 litros 
+Entre eixo: 2.500 
+Tanque: 40 litros 
+Ar condicionado 
+Direção hidráulica ou elétrica ou eletro hidráulica; `}
+            </p>
           </>
         ),
       },
